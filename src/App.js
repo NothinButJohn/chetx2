99faf8004051f376d0be2c8f2b726a16923a8a7b
NothinButJohn <jharkins2@wisc.edu>
2020-10-27 18:51:50 -0500

    redux intro

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React, { Component, useState } from 'react';
 
-
-function App() {
-  return (
-    <div className="App">
-
+function App(){
+  return(
+    <div>
+      <h1>App</h1>
+      <Counter />
     </div>
-  );
+  )
+}
+
+function Counter() {
+  const [counter, setCounter] = useState(0); // State: counter value, init 0
+  const increment = () => { // Action: updates the counter 
+    setCounter(prevCounter => prevCounter + 1);
+  }
+  // View: UI def
+  return (
+    <div>
+      Value: {counter} <button onClick={increment}>Increment</button>
+    </div>
+  )
 }
 
 export default App;
